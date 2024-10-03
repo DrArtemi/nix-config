c2250f19d74a9a27a009879063441494615e8428
DrArtemi <grinalp@proton.me>
2024-10-03 13:58:22 +0200

    add default profile to thunderbird

--- modules/home-manager/gui-programs/thunderbird.nix
+++ modules/home-manager/gui-programs/thunderbird.nix
@@ -1,5 +1,11 @@
 {inputs, pkgs, ...}: {
   programs.thunderbird = {
     enable = true;
+    profiles = {
+      default = {
+        isDefault = true;
+        withExternalGnupg = true;
+      };
+    };
   };
 }
